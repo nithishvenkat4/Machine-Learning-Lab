96e67d0823c9d74c56fa4ec0f3c370d7b6498418
nithishvenkat4 <nithishvenkat4507@gmail.com>
2025-12-07 15:07:58 +0530

    2nd ex first completion

--- EX-2/pg1.py
+++ EX-2/pg1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data=pd.read_csv("C:\\Users\\admin\\Desktop\\SEM-IV\\ml-lab\\EX-2\\loan_data.csv")
+data=pd.read_csv("A:\\Users\\Hp\\Documents\\PROGRAMMIING\\COLLEGE\\SEM-IV\\Machine-Learning-Lab\\EX-2\\loan_data.csv")
 
 cor=data[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount','Loan_Amount_Term', 'Credit_History']].corr()
 sns.heatmap(cor,annot=True,cmap='coolwarm')
@@ -17,6 +17,11 @@ plt.title('Applicant Income Vs Co-Aplicant Income')
 plt.show()
 
 #box plot for comkparing 2 incomes
-sns.boxplot(data=data,x='ApplicantIncome',y='CoapplicantIncome')
+sns.boxplot(data=data,x='Education',y='CoapplicantIncome')
 plt.title('Applicant Income Vs Co-Aplicant Income')
+plt.show()
+
+#histplot
+sns.histplot(data=data[['ApplicantIncome','CoapplicantIncome','LoanAmount']],kde=True)
+plt.title("Incomes and Loan Amount")
 plt.show()
