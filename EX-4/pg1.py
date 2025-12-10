797d9a062cb96654baecda30c84de3c7593acd87
nithishvenkat4 <nithishvenkat4507@gmail.com>
2025-12-10 15:04:41 +0530

    First push

---
+++ EX-4/pg1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+data=pd.read_csv("C:\\Users\\admin\\Desktop\\SEM-IV\\Machine-Learning-Lab\\EX-4\\marketing_campaign.csv")
